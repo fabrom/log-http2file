de61ba6d9e897c4dd33bf89b0f5ec1dd98407ffa
Fabrice ROMAND <fabrice.romand@gmail.com>
2016-05-27 09:12:22 +0200

    introduce command line log-http2file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,31 @@
-# 2016-05-25 - 0.1.1
+# ChangeLog
+
+## 2016-05-27 - 0.2.0
+
+- introduce command line _log-http2file_
+- options and arguments management
+```sh
+log-http2file v.0.2.0 - Simple HTTP server to log POST request content to file
+Author: Fabrice Romand <fabrice.romand@gmail.com> - Homepage: https://github.com/fabrom/log-http2file
+Report bugs on https://github.com/fabrom/log-http2file/issues
+
+  Usage: log-http2file [options] <outputfile>
+
+  Options:
+
+    -h, --help         output usage information
+    -V, --version      output the version number
+    -w, --workers <n>  how many listening workers
+    -p, --port <n>     listening port
+```
+
+
+## 2016-05-26 - 0.1.2
+
+- manage signal SIGHUP (close output file, rename output file and restart workers)
+- manage signals SIGINT, SIGTERM and SIGBREAK
+
+## 2016-05-25 - 0.1.1
 
 - manage string message and JSON data
 - include remote info (user, agent, address) in JSON data
--- server.js
+++ server.js
@@ -15,12 +15,13 @@ const numCPUs = require('os').cpus().length;
 const tmpdir = require('os').tmpdir();
 const path = require('path');
 
+
 // Variables declaration ----
 var prg_package = null;
 var port = (process.env.npm_package_config_port) ? process.env.npm_package_config_port : 8142;
-var nb_workers = (numCPUs/2)+1
-var tmplogfile = path.join(tmpdir, 'message.log');
-var output_file = (process.argv.length > 2) ? process.argv[process.argv.length - 1] : tmplogfile;
+var nb_workers = (numCPUs/2)+1; 
+var output_file = path.join(tmpdir, 'message.log');
+
 
 // Retrieve request source address
 function getRemoteAddress(req) {
@@ -57,90 +58,171 @@ function isJSON(jsonString) {
     return json;
 }
 
-// Main -----------------------------------------------------------------------
+// Go through all workers
+function eachWorker(callback) {
+  for (var id in cluster.workers) {
+    callback(cluster.workers[id]);
+  }
+}
 
-if (cluster.isMaster) {
-
-    prg_package = JSON.parse(fs.readFileSync(path.join(__dirname,'package.json'), 'utf8'));
-    console.log(prg_package.name + " v." + prg_package.version + ' - ' + prg_package.description);
-    console.log("Author: " + prg_package.author + " - Homepage: " + prg_package.homepage);
-    console.log("Report bugs on " + prg_package.bugs);
-    
-    nb_workers = (process.argv.indexOf('-w')>-1) ? process.argv[process.argv.indexOf('-w') + 1] : nb_workers;
+// Fork cluster
+function setWorkers() {
     for (var i = 1; i <= nb_workers; i++) {
         cluster.fork();
     }
-    
-    port = (process.argv.indexOf('-p')>-1) ? process.argv[process.argv.indexOf('-p') + 1] : port;
-    console.log(`${nb_workers} workers listening on port ${port} and writing to ${output_file}...`);
-
-    cluster.on('exit', (worker, code, signal) => {
-      console.log(`worker ${worker.process.pid} exit with code: ${code} (signal: ${signal})`);
-    });
-
-} else {
-
-    fs.open(output_file, 'a', function(err, flogd) {
-
-        if (err) {
-            console.log("can't open output file");
-            cluster.worker.kill();
-        } else {
-            const server = http.createServer((request, response) => {
-                if (request.method !== 'POST') {
-                    response.statusCode = 405;
-                    response.end();
-                } else {
-                    var body = '';
-                    request.on('data', function (data) {
-                        body += data;
-                        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-                        if (body.length > 1e6) {
-                            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-                            request.connection.destroy();
-                        }
-                    });
-                    request.on('end', function () {
-                        var remote_address = getRemoteAddress(request);
-                        var remote_user = getRemoteUser(request);
-                        var body_object = isJSON(body);
-                        var remote_object = { 
-                                address: remote_address,
-                                user: remote_user,
-                                agent: getRemoteAgent(request)
-                        };
-                        if (body_object instanceof Object) {
-                            body_object.remote = remote_object;
-                            body = JSON.stringify(body_object);
-                        };
-                        fs.write(flogd, body + '\n', function() {
-                            response.statusCode = 200;
-                            response.end();
-                        });
-                        var continuation = (body.length > 128) ? "..." : "";
-                        var message = body.substr(1, 128);
-                        console.log(Date.now() +
-                            " [" + remote_address +
-                            "][" + cluster.worker.process.pid + 
-                            "] " + message + continuation
-                        );
-                    });
-
-                }
-            });
-
-            server.on('clientError', (err, socket) => {
-                socket.statusCode = 400;
-                socket.end();
-            });
-
-            server.on('close', (err, socket) => {
-                console.log('closing server...');
-                fs.close(flogd);
-            });
-
-            server.listen(port);
-
-        }
-    });
+    console.log(`Master process id: ${process.pid}`);
 }
+
+// Main -----------------------------------------------------------------------
+
+function startServer(onReadyCallBack) {
+    
+    if (cluster.isMaster) {
+        
+        const program = require('commander');
+        
+        prg_package = JSON.parse(fs.readFileSync(path.join(__dirname,'package.json'), 'utf8'));
+        console.log(prg_package.name + " v." + prg_package.version + ' - ' + prg_package.description);
+        console.log("Author: " + prg_package.author + " - Homepage: " + prg_package.homepage);
+        console.log("Report bugs on " + prg_package.bugs);
+        
+        program
+            .version(prg_package.version)
+            .usage('[options] <outputfile>')
+            .option('-w, --workers <n>', 'how many listening workers', parseInt)
+            .option('-p, --port <n>', 'listening port', parseInt)
+            .arguments('<outputfile>')
+                .action(function (outputfile) {
+                    output_file = outputfile;
+                })
+            .parse(process.argv);
+            
+        nb_workers = program.workers || nb_workers;
+        port = program.port || port;
+        
+        cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} exit with code: ${code} (signal: ${signal})`);
+        });
+        
+        cluster.on('listening', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} now listening on port ${port} and writing to ${output_file}...`);
+        });
+        
+        process.on('SIGHUP', function() {
+            eachWorker(function(worker) {
+                worker.kill('SIGHUP');
+            })
+            var d = new Date();
+            var d1 = d.toISOString();
+            var d2 = d1.substr(0,19).replace(/-/g,'').replace(/:/g,'').replace('T','');
+            var newdir = path.dirname(output_file);
+            var newname = path.basename(output_file, path.extname(output_file)) + '_' + d2 + path.extname(output_file);
+            fs.rename(output_file, path.join(newdir, newname), function() {
+                console.log(`${path.basename(output_file)} renamed to ${newname}`);
+                setWorkers();            
+            });
+        });
+        
+        process.on('SIGINT', function() {
+            eachWorker(function(worker) {
+                worker.kill('SIGINT');
+            });
+        });
+        
+        process.on('SIGTERM', function() {
+            eachWorker(function(worker) {
+                worker.kill('SIGTERM');
+            });
+        });
+        
+        process.on('SIGBREAK', function() {
+            eachWorker(function(worker) {
+                worker.kill('SIGBREAK');
+            });
+        });
+            
+        setWorkers();
+        
+        if (onReadyCallBack) onReadyCallBack();
+
+    } else {
+
+        fs.open(output_file, 'a', function(err, flogd) {
+
+            if (err) {
+                console.error("can't open output file");
+                cluster.worker.kill();
+            } else {
+                const server = http.createServer((request, response) => {
+                    if (request.method !== 'POST') {
+                        response.statusCode = 405;
+                        response.end();
+                    } else {
+                        var body = '';
+                        request.on('data', function (data) {
+                            body += data;
+                            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+                            if (body.length > 1e6) {
+                                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                                request.connection.destroy();
+                            }
+                        });
+                        request.on('end', function () {
+                            var remote_address = getRemoteAddress(request);
+                            var remote_user = getRemoteUser(request);
+                            var body_object = isJSON(body);
+                            var remote_object = { 
+                                    address: remote_address,
+                                    user: remote_user,
+                                    agent: getRemoteAgent(request)
+                            };
+                            if (body_object instanceof Object) {
+                                body_object.remote = remote_object;
+                                body = JSON.stringify(body_object);
+                            };
+                            fs.write(flogd, body + '\n', function(err, written, string) {
+                                if (err) {
+                                    console.error("can't write to output file");
+                                }
+                                response.statusCode = 200;
+                                response.end();
+                            });
+                            var continuation = (body.length > 128) ? "..." : "";
+                            var message = body.substr(0, 128);
+                            console.log(Date.now() +
+                                " [" + remote_address +
+                                "][" + cluster.worker.process.pid + 
+                                "] " + message + continuation
+                            );
+                        });
+
+                    }
+                });
+
+                server.on('clientError', (err, socket) => {
+                    socket.statusCode = 400;
+                    socket.end();
+                });
+
+                server.on('close', (err, socket) => {
+                    fs.close(flogd);
+                });
+            
+                server.listen(port);
+            }
+        });
+    }
+}
+
+function stopServer() {
+    process.kill(process.pid, 'SIGTERM');
+}
+
+function renewServer() {
+    process.kill(process.pid, 'SIGHUP');
+}
+
+module.exports = exports
+exports.start = startServer;
+exports.stop = stopServer;
+exports.renew = renewServer;
